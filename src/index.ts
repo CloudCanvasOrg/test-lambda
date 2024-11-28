b66be644eb98d070412a16ee4eb4a9489e84e478
Magnus Reeves <rreeves8@uwo.ca>
2024-11-27 21:27:49 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 
 export async function handler(event: APIGatewayProxyEventV2) {
+  console.log('running');
   return {
     statusCode: 200,
     body: JSON.stringify({
