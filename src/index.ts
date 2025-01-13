f3c40691cbf43cf28874628afa422f920d8e00d6
Magnus Reeves <rreeves8@uwo.ca>
2025-01-12 19:48:51 -0500

    add content type

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import { APIGatewayProxyEventV2 } from "aws-lambda";
+import {
+  APIGatewayProxyEventV2,
+  APIGatewayProxyStructuredResultV2,
+} from "aws-lambda";
 
-export async function handler(event: APIGatewayProxyEventV2) {
-  console.log('running');
+export async function handler(
+  event: APIGatewayProxyEventV2
+): Promise<APIGatewayProxyStructuredResultV2> {
+  console.log("running");
   return {
     statusCode: 200,
+    headers: { "Content-Type": "text/html" },
     body: "<div>Test lambda</div>",
   };
 }
